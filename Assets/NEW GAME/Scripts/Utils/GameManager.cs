3103d886421ab79c735e9b36b6c9af2b8e801270
Clement <clement.roth@live.fr>
2019-09-11 10:16:28 +0200

    Ajout base projet

---
+++ Assets/NEW GAME/Scripts/Utils/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LibLabSystem;
+using UnityEngine;
+using PathologicalGames;
+using TMPro;
+using DG.Tweening;
+
+namespace LibLabGames.NewGame
+{
+    public class GameManager : IGameManager
+    {
+        public static GameManager instance;
+
+        public override void GetSettingGameValues()
+        {
+            // Example :
+            // int value = settingValues.GetIntValue("exampleThree");
+        }
+
+        private void Awake()
+        {
+            if (!DOAwake()) return;
+
+            instance = this;
+        }
+
+        private void Start()
+        {
+            // DOStart can be place as you wish
+            base.DOStart();
+        }
+    }
+}
